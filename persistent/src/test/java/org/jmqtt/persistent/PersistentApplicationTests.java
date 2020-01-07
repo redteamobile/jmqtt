9e2010ba64cfaef71e5c28179b1443099e7dbe18
LTHRedteaMobile <tianheng.liu@redteamobile.com>
2020-01-07 16:09:23 +0800

    去除无用的包

--- persistent/src/test/java/org/jmqtt/persistent/PersistentApplicationTests.java
+++ persistent/src/test/java/org/jmqtt/persistent/PersistentApplicationTests.java
@@ -1,11 +1,8 @@
 package org.jmqtt.persistent;
 
-import org.jmqtt.persistent.entity.Client;
-import org.jmqtt.persistent.service.ClientService;
 import org.jmqtt.persistent.utils.TestUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
