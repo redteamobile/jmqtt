19e6d9cef19f8cad61998b2aaa630c55b2111c0c
LTHRedteaMobile <tianheng.liu@redteamobile.com>
2020-01-20 16:42:55 +0800

    修改username和password位数

--- persistent/src/main/java/org/jmqtt/persistent/service/TicketService.java
+++ persistent/src/main/java/org/jmqtt/persistent/service/TicketService.java
@@ -1,5 +1,6 @@
 package org.jmqtt.persistent.service;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jmqtt.persistent.model.page.SingleChannelDeviceResp;
 import org.jmqtt.persistent.model.req.YunBaTicketReq;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class TicketService {
 
-    private final String USERNAME = "redtea";
-    private final String PASSWORD = "redtea123";
+    private final String USERNAME = "3217011410455896576";
+    private final String PASSWORD = "21b9c374e1d39";
 
     public SingleChannelDeviceResp servForSingleChannelDevice(YunBaTicketReq yunBaTicketReq){
         SingleChannelDeviceResp singleChannelDeviceResp = new SingleChannelDeviceResp();
@@ -17,7 +18,11 @@ public class TicketService {
         singleChannelDeviceResp.setD(yunBaTicketReq.getD());
         if(yunBaTicketReq.getD().length() > 23){
             singleChannelDeviceResp.setC(
-                    yunBaTicketReq.getD().substring(yunBaTicketReq.getD().length() - 24 , yunBaTicketReq.getD().length()-1));
+                    yunBaTicketReq.getD().substring(yunBaTicketReq.getD().length() - 23 , yunBaTicketReq.getD().length()));
+        }else if(yunBaTicketReq.getD().length() < 23){
+            int padding = 23 - yunBaTicketReq.getD().length();
+            String paddingString = RandomStringUtils.randomAlphabetic(padding);
+            singleChannelDeviceResp.setC(yunBaTicketReq.getD() + paddingString);
         }else{
             singleChannelDeviceResp.setC(yunBaTicketReq.getD());
 
