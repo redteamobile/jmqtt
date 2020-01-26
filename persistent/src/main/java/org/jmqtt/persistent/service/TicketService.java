11732f0299e5bf27a5b2b1f6b3b318870f63b249
LTHRedteaMobile <tianheng.liu@redteamobile.com>
2020-01-26 17:00:15 +0800

    fix

--- persistent/src/main/java/org/jmqtt/persistent/service/TicketService.java
+++ persistent/src/main/java/org/jmqtt/persistent/service/TicketService.java
@@ -10,6 +10,7 @@ public class TicketService {
 
     private final String USERNAME = "3217011410455896576";
     private final String PASSWORD = "21b9c374e1d39";
+    private final static String DEFAULT_STRING = "qwertyuiopasdfghjklzxcvbnm";
 
     public SingleChannelDeviceResp servForSingleChannelDevice(YunBaTicketReq yunBaTicketReq){
         SingleChannelDeviceResp singleChannelDeviceResp = new SingleChannelDeviceResp();
@@ -22,7 +23,7 @@ public class TicketService {
         }else if(yunBaTicketReq.getD().length() < 23){
             //通过计算D的hash值来补全到23位，这样确保设备每次请求获取到的clientId都是相同的
             int padding = 23 - yunBaTicketReq.getD().length();
-            String paddingString = (yunBaTicketReq.getD().hashCode() + "").substring(0 , padding);
+            String paddingString = DEFAULT_STRING.substring(0 , padding);
             singleChannelDeviceResp.setC(yunBaTicketReq.getD() + paddingString);
         }else{
             singleChannelDeviceResp.setC(yunBaTicketReq.getD());
